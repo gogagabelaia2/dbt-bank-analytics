3f17f57176894f38d3f94f6057555cc0e59366c2
gogagabelaia2 <goga.gabelaia2@gmail.com>
2026-05-28 12:56:52 +0400

    feat: add intermediate models

--- models/intermediate/int_loan_details.sql
+++ models/intermediate/int_loan_details.sql
@@ -8,10 +8,53 @@ with loans as(
         classification
     from {{ref('stg_loans')}}
 ),
-logic as(
-    select 
-        round(sum(original_amount) filter(where classification in ('Substandard', 'Doubtful', 'Loss'))
-        *100.0 /sum(original_amount),2) as npl_ratio
-        from loans
+classifications as(  
+    SELECT  
+        classification,   
+        risk_level,
+        provision_rate
+    from  {{ref('loan_classifications')}} 
+),
+collateral as(
+    select
+        collateral_id,
+        loan_id,
+        customer_id,
+        lower(trim(collateral_type)) as collateral_type,
+        current_value,
+        created_at
+    from {{ref('stg_collateral')}}
+),
+collateral_summary as( 
+    select
+        loan_id, 
+        sum(current_value) as total_value,
+        count(*) as total_collateral
+    from collateral
+    GROUP BY loan_id
+),
+final as(
+    select
+        l.loan_id,
+        l.loan_type,
+        l.original_amount,
+        l.outstanding_balance,
+        l.days_past_due,
+        l.classification,
+        {{ classify_loan_risk('l.days_past_due', 'l.classification') }} as risk_category,
+        {{ dpd_bucket('l.days_past_due') }} as dpd_bucket,
+        c.risk_level,
+        c.provision_rate,
+        l.outstanding_balance * c.provision_rate as provision_amount,
+        cs.total_value  as total_collateral_value,
+        cs.total_collateral,
+        {{ safe_divide('cs.total_value', 'l.outstanding_balance') }} as collateral_coverage_ratio,
+        case
+            when l.classification in ('Substandard', 'Doubtful', 'Loss')
+            then 1 else 0
+        end as is_non_performing
+    from loans l
+    left join classifications c on l.classification = c.classification
+    left join collateral_summary cs on l.loan_id = cs.loan_id
 )
-select * from logic
+select * from final
