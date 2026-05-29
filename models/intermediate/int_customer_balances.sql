e7a59eb57d425753873167152698be95fbc0c055
gogagabelaia2 <goga.gabelaia2@gmail.com>
2026-05-29 20:47:56 +0400

    add mart models

--- models/intermediate/int_customer_balances.sql
+++ models/intermediate/int_customer_balances.sql
@@ -24,24 +24,36 @@ accounts as(
 logic as(
     SELECT
         c.customer_id,
-        count(*) as total_accounts,
+        c.customer_type,
+        c.country_code,
+        c.city,
+        c.onboarding_date,
+        count(ac.account_id) as total_accounts,
         sum(ac.balance) as total_balance,
         case when count(l.loan_id) > 0 then true else false end as has_loan,
         sum(l.outstanding_balance) as total_outstanding_debt
     from customers c
-    left join loans l on c.customer_id=l.customer_id
-    left join accounts ac on c.customer_id=ac.customer_id
-    group by 1
-
+    left join loans l on c.customer_id = l.customer_id
+    left join accounts ac on c.customer_id = ac.customer_id
+    group by
+        c.customer_id,
+        c.customer_type,
+        c.country_code,
+        c.city,
+        c.onboarding_date
 ),
 final as(
     select
         customer_id,
+        customer_type,
+        country_code,
+        city,
+        onboarding_date,
         total_accounts,
         total_balance,
         has_loan,
         total_outstanding_debt,
-       {{customer_segment('total_balance')}} as customer_segmented
+        {{ customer_segment('total_balance') }} as customer_segmented
     from logic
 )
 select * from final
