ecc738c640d28b73982717986bf869a134762aa2
gogagabelaia2 <goga.gabelaia2@gmail.com>
2026-05-30 12:55:13 +0400

    add marts schema tests

--- models/intermediate/int_customer_balances.sql
+++ models/intermediate/int_customer_balances.sql
@@ -29,9 +29,9 @@ logic as(
         c.city,
         c.onboarding_date,
         count(ac.account_id) as total_accounts,
-        sum(ac.balance) as total_balance,
+        coalesce(sum(ac.balance),0) as total_balance,
         case when count(l.loan_id) > 0 then true else false end as has_loan,
-        sum(l.outstanding_balance) as total_outstanding_debt
+        coalesce(sum(l.outstanding_balance),2) as total_outstanding_debt
     from customers c
     left join loans l on c.customer_id = l.customer_id
     left join accounts ac on c.customer_id = ac.customer_id
